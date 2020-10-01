bdb55dd16c089dab41dde104651c8b6154c53053
Megan McNulty <megan.mcnulty07@gmail.com>
2020-10-01 15:43:31 -0400

    Splash Intro

--- frontend/components/splash/splash_intro.jsx
+++ frontend/components/splash/splash_intro.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const SplashIntro = props => {
+    const history = useHistory();
+
     return (
-        <div>
-            intro
+        <div className="intro">
+            <section>
+                <p>CHILL AT HOME</p>
+                <h1>Lax brings you together, wherever you are</h1>
+                <p>All of the communication tools you need to spend time with friends, plan time together, and embrace relaxation no matter where you are.</p>
+                <Link to="/signup">
+                    <button className="btn-blue">
+                        <span>TRY LAX FOR FREE</span>
+                    </button>
+                </Link>
+                <Link to="#"> 
+                    <button onClick={this.handleDemo}
+                        className="btn-white">
+                        <span>SEE THE DEMO</span>
+                    </button>
+                </Link>
+            </section>
         </div>
     )
 }
