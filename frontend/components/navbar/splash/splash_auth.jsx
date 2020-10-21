351e55452fde118f7c6847f12992e2d1789d2656
Megan McNulty <megan.mcnulty07@gmail.com>
2020-10-21 10:34:06 -0400

    early changes for larger screen

--- frontend/components/navbar/splash/splash_auth.jsx
+++ frontend/components/navbar/splash/splash_auth.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default props => (
-    <nav className="auth-nav">
-        <div className="nav-btns logged-out">
-            <Link to="/login">
-                Sign in
-            </Link>
-            <Link to="/signup">
-                <button className="btn-blue">
-                    GET STARTED
-                </button>
-            </Link>
-        </div>
+export default () => (
+    <nav className="nav-btns logged-out">
+        <Link to="/login">
+            Sign in
+        </Link>
+        <Link to="/signup">
+            <button className="btn-blue">
+                GET STARTED
+            </button>
+        </Link>
     </nav>
 )
